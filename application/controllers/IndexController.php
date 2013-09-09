57b2ed5a15d06d1c259e21924d9c1bcbe34c3ff2
ShineISP <info@shineisp.com>
2013-09-09 20:42:41 +0200

    Zend Framework Submodule added

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -10,23 +10,7 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        try{
-	    	// initialize SOAP client
-	    	$options = array(
-	    			'location' 	=> 'http://demo.shineisp.com/api/products.soap',
-	    			'uri'      	=> 'http://demo.shineisp.com/api/products.soap',
-	    			'login' 	=> 'api@shineisp.com',
-	    			'password' 	=> '123456789'
-	    	);
-	    	 
-	    	// action body
-	        $client = new Zend_Soap_Client(null, $options);
-	        $result = $client->get(array(1));
-	        Zend_Debug::dump($result);
-	        
-        }catch(SoapFault $e){
-        	echo $e->getMessage();
-        }
+        // action body
     }
 
 
--- docs/README.txt
+++ docs/README.txt
@@ -14,13 +14,13 @@ Setting Up Your VHOST
 The following is a sample VHOST you might want to consider for your project.
 
 <VirtualHost *:80>
-   DocumentRoot "/var/www/shineispclient/public"
-   ServerName shineispclient.local
+   DocumentRoot "/Library/WebServer/Documents/shineisp-connect/public"
+   ServerName shineisp-connect.local
 
    # This should be omitted in the production environment
    SetEnv APPLICATION_ENV development
 
-   <Directory "/var/www/shineispclient/public">
+   <Directory "/Library/WebServer/Documents/shineisp-connect/public">
        Options Indexes MultiViews FollowSymLinks
        AllowOverride All
        Order allow,deny
